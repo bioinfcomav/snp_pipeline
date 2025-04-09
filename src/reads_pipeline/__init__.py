5d7967a8d49687423af0b22f123332cafec99a2b
Jose Blanca <jblanca@upv.es>
2025-04-09 10:49:25 +0200

    allow parrallel mapping

--- src/reads_pipeline/__init__.py
+++ src/reads_pipeline/__init__.py
@@ -1,7 +1,7 @@
 from .read_stats import run_fastqc, collect_fastqc_stats
 from .fastp import run_fastp, collect_fastp_stats
 from .fastp_minimap import (
-    run_fastp_minimap,
+    run_fastp_minimap_for_fastqs,
     collect_cram_stats,
     plot_mapq_distributions,
     plot_coverage_distributions,
